6c78400dd424d7fefbdcc7f66a243bf9b5e28dc4
MalingasCumbane <malingascumbane1@gmail.com>
2025-05-23 07:45:45 +0200

    dsa

--- core/views.py
+++ core/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from core.serializers import CertificadoRegistoSerializer, PagamentoSerializer, RegistoCriminalSerializer, SolicitarRegistoSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
